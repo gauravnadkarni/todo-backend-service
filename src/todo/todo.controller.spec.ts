2c110713228763d5f2e35795722f359592b01731
Gaurav <nadkarnigaurav@gmail.com>
2024-09-27 19:05:10 +0530

    Added few test cases

--- src/todo/todo.controller.spec.ts
+++ src/todo/todo.controller.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
 
 describe('TodoController', () => {
+  const mockTodoService = {
+    addTodo: jest.fn(),
+    getTodoById: jest.fn(),
+    updateTodo: jest.fn(),
+    listAllTodosPaginated: jest.fn(),
+    deleteTodo: jest.fn(),
+    // Add other methods you need to mock
+  };
+  const mockTodoRepository = {
+    findOne: jest.fn(),
+    update: jest.fn(),
+    // Add other methods you need to mock
+  };
   let controller: TodoController;
+  let todoService: typeof mockTodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodoController],
+      providers:[TodoService,{
+        provide: 'TodoRepository', // Change this to the actual token you're using
+        useValue: mockTodoRepository,
+      },{
+        provide: 'TodoService', // Change this to the actual token you're using
+        useValue: mockTodoService,
+      }]
     }).compile();
 
     controller = module.get<TodoController>(TodoController);
--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -2,14 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
+  const mockTodoRepository = {
+    findOne: jest.fn(),
+    update: jest.fn(),
+    // Add other methods you need to mock
+  };
   let service: TodoService;
+  let todoRepository: typeof mockTodoRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      providers: [TodoService,{
+        provide: 'TodoRepository', // Change this to the actual token you're using
+        useValue: mockTodoRepository,
+      }],
     }).compile();
 
     service = module.get<TodoService>(TodoService);
+    //todoRepository = module.get('TodoRepository');
   });
 
   it('should be defined', () => {
